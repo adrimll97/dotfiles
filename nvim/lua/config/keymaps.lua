eec4d4490bf4e9fc8325d08312046d46d5b01de5
Adrian <adrigalileo@gmail.com>
2024-12-30 13:55:02 +0100

    Add new nvim config with lazy.vim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", ",s", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", ",e", ":qa!<CR>", { desc = "Exit neovim" })
+vim.keymap.set("n", ",q", ":q<CR>", { desc = "Close window" })
+vim.keymap.set("n", ",d", "<Leader>bd", { remap = true, desc = "Delete buffer" })
+vim.keymap.set("n", ",D", "<Leader>bD", { remap = true, desc = "Delete buffer and window" })
+vim.keymap.set("n", ",b", ":Git blame<CR>", { desc = "Blame file" })
+vim.keymap.set("n", ";", "gcc", { remap = true, desc = "Comment" })
+vim.keymap.set("v", ";", "gc", { remap = true, desc = "Comment selection" })
+vim.keymap.set("n", "t", "<Leader>e", { remap = true, desc = "Open Neotree" })
+vim.keymap.set("n", "s", "cl", { remap = true, desc = "Substitute text" })
+vim.keymap.set("v", "s", "c", { remap = true, desc = "Substitute text" })
+vim.keymap.set("n", ",gs", "<cmd>SplitjoinSplit<CR>", { remap = true, desc = "Split text" })
+vim.keymap.set("n", ",gj", "<cmd>SplitjoinJoin<CR>", { remap = true, desc = "Join text" })
+vim.keymap.set("n", ",a", ":A<CR>", { remap = true, desc = "Open alternative file" })
+vim.keymap.set("n", ",r", ":R<CR>", { remap = true, desc = "Open related file" })
+vim.keymap.set("n", ",z", "<Leader>uz", { remap = true, desc = "Open file in zen mode" })
+vim.keymap.set("n", ",t", "<Leader><tab><tab>", { remap = true, desc = "Open new tab" })
+vim.keymap.set("n", "<c-p>", "<Leader><Leader>", { remap = true, desc = "Find files" })
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "sainnhe/gruvbox-material" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "gruvbox-material",
+    },
+  },
+}
