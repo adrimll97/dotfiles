3971b4cf2978111b3a75b856c2d3d02f642c44b8
Adrian <adrigalileo@gmail.com>
2025-06-11 20:16:13 +0200

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -21,3 +21,14 @@ vim.keymap.set("n", ",r", ":R<CR>", { remap = true, desc = "Open related file" }
 vim.keymap.set("n", ",z", "<Leader>uz", { remap = true, desc = "Open file in zen mode" })
 vim.keymap.set("n", ",t", "<Leader><tab><tab>", { remap = true, desc = "Open new tab" })
 vim.keymap.set("n", "<c-p>", "<Leader><Leader>", { remap = true, desc = "Find files" })
+vim.keymap.set("n", "<c-j>", ":m .+1<CR>==", { remap = true, desc = "Move line up" })
+vim.keymap.set("n", "<c-k>", ":m .-2<CR>==", { remap = true, desc = "Move line down" })
+vim.keymap.set("v", "<c-j>", ":m '>+1<CR>gv=gv", { remap = true, desc = "Move line up in v mode" })
+vim.keymap.set("v", "<c-k>", ":m '<-2<CR>gv=gv", { remap = true, desc = "Move line down in v mode" })
+vim.keymap.set("n", "<Leader>rr", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>", { noremap = true, desc = "Rename" })
+vim.keymap.set(
+  "n",
+  "<Leader>rR",
+  ":%s/<C-r><C-w>/<C-r><C-w>/gc<Left><Left><Left>",
+  { noremap = true, desc = "Rename with confirmation" }
+)
