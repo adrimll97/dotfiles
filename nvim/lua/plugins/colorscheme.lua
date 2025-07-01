98443ba50c1555794c02b25de53581760fa6fe9f
Adrian <adrigalileo@gmail.com>
2025-07-01 13:18:58 +0200

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "sainnhe/gruvbox-material" },
+  { "rose-pine/neovim", name = "rose-pine" },
   {
     "LazyVim/LazyVim",
     opts = {
